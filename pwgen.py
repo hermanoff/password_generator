a209c69c413fe6c73277cbfbadb8f88f0fa8e1b8
Viktor Herman <vherman@tria.hr>
2023-08-22 13:40:23 +0200

    Automatically copy password to clipboard

--- pwgen.py
+++ pwgen.py
@@ -1,6 +1,7 @@
 import secrets
 import string
 import sys
+import pyperclip
 
 letters = string.ascii_letters
 digits = string.digits
@@ -14,8 +15,13 @@ def generate_password(length, char_range):
             for i in range(length):
                 pwd += ''.join(secrets.choice(char_range))
             print(f'Generated password:\n{pwd}')
+            return pwd
         except:
             print('There was an error!')
+
+
+def copy_to_clipboard(text):
+    pyperclip.copy(text)
 
 
 def main():
@@ -28,12 +34,12 @@ def main():
     if args[0] != '--charonly':
         pwd_length = int(args[0])
         alphabet = letters + digits + special_chars
-        generate_password(pwd_length, alphabet)
+        copy_to_clipboard(generate_password(pwd_length, alphabet))
 
     if args[0] == '--charonly':
         pwd_length = int(args[1])
         alphabet = letters + digits
-        generate_password(pwd_length, alphabet)
+        copy_to_clipboard(generate_password(pwd_length, alphabet))
 
 
 if __name__ == '__main__':
