799444d3e2f78fa3cb846719f1cc49f11fa8ae53
Viktor Herman <viktor.herman@vh-ws.local>
2024-05-14 17:30:32 +0200

    Refactor - remove copy_to_clipboard(), remove try: and except: blocks; add docstring and type hints

--- pwgen.py
+++ pwgen.py
@@ -8,42 +8,43 @@ digits = string.digits
 special_chars = string.punctuation
 
 
-def generate_password(length, char_range):
+def generate_password(char_range: str, length: int = 32) -> str | sys.exit:
+    """Password generator, for given alphabet and length
+
+    Args:
+        char_range (str): Character range, --charonly makes password of only letters and digits
+        length (int, optional): Desired password length. Defaults to 32.
+
+    Returns:
+        str | sys.exit: Either returns string or exits the program
+    """
     if length >= 8:
-        pwd = ''
-        try:
-            for i in range(length):
-                pwd += ''.join(secrets.choice(char_range))
-            print(f'Generated password:\n{pwd}')
-            return pwd
-        except:
-            print('There was an error!')
+        password = ""
+        for i in range(length):
+            password += "".join(secrets.choice(char_range))
+        return password
     else:
-        print('Password should be at least 8 characters long')
+        print("Password should be at least 8 characters long")
         sys.exit(1)
-
-
-def copy_to_clipboard(text):
-    pyperclip.copy(text)
 
 
 def main():
     args = sys.argv[1:]
 
     if not args:
-        print('Usage: [--charonly] password_length')
+        print("Usage: [--charonly] password_length")
         sys.exit(1)
 
-    if args[0] != '--charonly':
+    if args[0] != "--charonly":
         pwd_length = int(args[0])
         alphabet = letters + digits + special_chars
-        copy_to_clipboard(generate_password(pwd_length, alphabet))
+        pyperclip.copy(generate_password(pwd_length, alphabet))
 
-    if args[0] == '--charonly':
+    if args[0] == "--charonly":
         pwd_length = int(args[1])
         alphabet = letters + digits
-        copy_to_clipboard(generate_password(pwd_length, alphabet))
+        pyperclip.copy(generate_password(pwd_length, alphabet))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
